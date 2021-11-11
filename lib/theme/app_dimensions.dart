f58ed8accad39e80997cc51b5348ed56a8edf04b
Atamyrat Babayev <atashka_babaev@mail.ru>
2021-11-11 15:36:38 +0500

    added separated dimensions class

---
+++ lib/theme/app_dimensions.dart
@@ -0,0 +1,13 @@
+class AppDimensions {
+  AppDimensions._();
+
+  static const borderRadiusMicro = 5.0;
+  static const borderRadiusSmall = 10.0;
+  static const borderRadiusMedium = 15.0;
+  static const borderRadiusLarge = 20.0;
+
+  static const paddingSmall = 10.0;
+  static const paddingMedium = 15.0;
+  static const paddingLarge = 17.0;
+  static const paddingExtraLarge = 20.0;
+}
