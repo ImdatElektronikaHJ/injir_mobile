1534bcac0f790e7bcaf3b5dcce7a5bd764f8c8da
Atamyrat Babayev <atashka_babaev@mail.ru>
2021-11-11 15:35:48 +0500

    added separated colors class

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color darkPurpleColor = Color.fromRGBO(59, 36, 90, 1);
+  static const Color darkPurpleColor40 = Color.fromRGBO(168, 157, 180, 1);
+  static const Color redColor = Color.fromRGBO(242, 17, 17, 1);
+  static const Color blackColor = Color.fromRGBO(52, 52, 54, 1);
+  static const Color darkerGreyColor = Color.fromRGBO(186, 186, 186, 1);
+  static const Color lightGreyColor = Color.fromRGBO(245, 245, 245, 1);
+  static const Color lightBlueGreyColor30 = Color.fromRGBO(250, 253, 254, 1);
+  static const Color lightBlueGreyColor = Color.fromRGBO(243, 247, 251, 1);
+  static const Color greenColor = Color.fromRGBO(13, 184, 90, 1);
+  static const Color greenColor05 = Color.fromRGBO(242, 252, 246, 1);
+  static const Color greenColor70 = Color.fromRGBO(13, 184, 90, 0.7);
+  static const Color lightGreyColor10 = Color.fromRGBO(28, 28, 28, 0.1);
+  static const Color lightGreyColor50 = Color.fromRGBO(28, 28, 28, 0.5);
+  static const Color lightPurpleColor35 = Color.fromRGBO(59, 36, 90, 0.35);
+  static const Color lighterPurpleColor25 = Color.fromRGBO(74, 54, 169, 0.25);
+  static const Color redColor60 = Color.fromRGBO(242, 17, 17, 0.6);
+  static const Color greyWhiteColor = Color.fromRGBO(251, 252, 254, 1);
+  static const Color orangeColor = Color.fromRGBO(252, 115, 43, 1);
+}
