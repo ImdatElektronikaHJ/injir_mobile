7c70c3ff16a13102a2360b9e217404bad06fcd38
Atamyrat Babayev <atashka_babaev@mail.ru>
2021-11-26 15:40:21 +0500

    added nested navigation ids

---
+++ lib/const/nested_navigation_ids.dart
@@ -0,0 +1,9 @@
+class NestedNavigationIds {
+  NestedNavigationIds._();
+  
+  static const int home = 0;
+  static const int categories = 1;
+  static const int cart = 2;
+  static const int stores = 3;
+  static const int profile = 4;
+}
