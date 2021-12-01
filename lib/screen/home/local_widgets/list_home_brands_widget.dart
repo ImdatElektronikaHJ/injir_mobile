c1a13b95811181564c83b9a2cd4f044e26afb06b
purpurni <sekisPurp@gmail.com>
2021-12-01 16:47:11 +0500

    list home brands widget created

---
+++ lib/screen/home/local_widgets/list_home_brands_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:tajir/theme/app_dimensions.dart';
+
+class ListHomeBrandsWidget extends StatelessWidget {
+  const ListHomeBrandsWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height:
+          AppDimensions.homeCategoriesHeight + AppDimensions.marginExtraLarge,
+      child: ListView.builder(
+        itemCount: 6,
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          return const HomeBrandsWidget();
+        },
+      ),
+    );
+  }
+}
+
+class HomeBrandsWidget extends StatelessWidget {
+  const HomeBrandsWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: AppDimensions.homeBrandsHeight + AppDimensions.marginSmall,
+      padding: const EdgeInsets.all(AppDimensions.marginSmall),
+      child: Image.asset('assets/images/bn1.png'),
+    );
+  }
+}
