507bcf9a8519f5743606b4a54ecca8cbc0fe40cf
purpurni <sekisPurp@gmail.com>
2021-12-02 21:43:33 +0500

    block methods moved to stateless widget

--- lib/widget/list_product_widget.dart
+++ lib/widget/list_product_widget.dart
@@ -30,60 +30,128 @@ class ListProductWidget extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Flexible(flex: 3, child: imageBlock(context)),
-          ratingBlock(context),
-          titleBlock(context),
-          priceBlock(context),
+          Flexible(
+              flex: 3,
+              child: ListProductImageBlock(
+                imageUrl: 'assets/placeholder/samsung.png',
+                onFavouriteTapped: (bool isInWishlist) {
+                  print(isInWishlist);
+                },
+                salePercentage: '123',
+              )),
+          const ListProductRatingBlock(
+            rating: 1.6,
+          ),
+          const ListProductTitleBlock(
+            title: 'Смартфон Samsung Galaxy Note 10+ 12/256GB White(SM-N975FZWDSER)',
+          ),
+          const ListProductPriceBlock(
+            price: '123123',
+          ),
         ],
       ),
     );
   }
+}
 
-  Stack imageBlock(BuildContext context) {
+class ListProductPriceBlock extends StatelessWidget {
+  final String price;
+  final String? salePrice;
+
+  const ListProductPriceBlock({Key? key, required this.price, this.salePrice})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: AppDimensions.paddingSmall,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '$price TMT',
+            textAlign: TextAlign.left,
+            style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                color: AppColors.darkBlueColor, fontWeight: FontWeight.bold),
+          ),
+          Visibility(
+            visible: salePrice != null && salePrice?.isNotEmpty == true,
+            child: Text('$salePrice TMT',
+                style: Theme.of(context)
+                    .textTheme
+                    .caption!
+                    .copyWith(decoration: TextDecoration.lineThrough)),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ListProductImageBlock extends StatelessWidget {
+  final String imageUrl;
+  final Function(bool) onFavouriteTapped;
+  final String? salePercentage;
+
+  const ListProductImageBlock(
+      {Key? key,
+      required this.imageUrl,
+      required this.onFavouriteTapped,
+      this.salePercentage})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
     return Stack(
       children: [
         Padding(
           padding: const EdgeInsets.only(top: AppDimensions.paddingSmall),
           child: Center(
-            child: Image.asset('assets/images/samsung.png'),
+            child: Image.asset(imageUrl),
           ),
         ),
         Align(
           alignment: Alignment.topRight,
           child: AnimatedLikeButton(
-            isInWishList: true,
-            onFavoriteTapped: (bool isInWishlist) {
-              print(isInWishlist);
-            },
-          ),
+              isInWishList: true, onFavoriteTapped: onFavouriteTapped),
         ),
-        Align(
-          alignment: Alignment.bottomRight,
-          child: Container(
-            margin: const EdgeInsets.symmetric(
-                horizontal: AppDimensions.paddingSmall),
-            decoration: BoxDecoration(
-              color: AppColors.redColor,
-              borderRadius:
-                  BorderRadius.circular(AppDimensions.borderRadiusSmall / 2),
-            ),
-            padding: const EdgeInsets.symmetric(
-                horizontal: AppDimensions.paddingSmall / 1.5,
-                vertical: AppDimensions.paddingSmall / 3),
-            child: Text(
-              '-20%',
-              maxLines: 1,
-              style: Theme.of(context).textTheme.button!.copyWith(
-                  color: AppColors.whiteColor, fontWeight: FontWeight.bold),
+        Visibility(
+          visible: salePercentage != null && salePercentage?.isNotEmpty == true,
+          child: Align(
+            alignment: Alignment.bottomRight,
+            child: Container(
+              margin: const EdgeInsets.symmetric(
+                  horizontal: AppDimensions.paddingSmall),
+              decoration: BoxDecoration(
+                color: AppColors.redColor,
+                borderRadius:
+                    BorderRadius.circular(AppDimensions.borderRadiusSmall / 2),
+              ),
+              padding: const EdgeInsets.symmetric(
+                  horizontal: AppDimensions.paddingSmall / 1.5,
+                  vertical: AppDimensions.paddingSmall / 3),
+              child: Text(
+                '-$salePercentage%',
+                maxLines: 1,
+                style: Theme.of(context).textTheme.button!.copyWith(
+                    color: AppColors.whiteColor, fontWeight: FontWeight.bold),
+              ),
             ),
           ),
         ),
       ],
     );
   }
+}
 
-  Center ratingBlock(BuildContext context) {
-    var rating = 3.5;
+class ListProductRatingBlock extends StatelessWidget {
+  final double? rating;
+  final int? rateTotal;
+
+  const ListProductRatingBlock({Key? key, this.rating = 0, this.rateTotal = 0})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
     return Center(
       child: Container(
         padding: const EdgeInsets.all(AppDimensions.paddingSmall / 3),
@@ -94,7 +162,7 @@ class ListProductWidget extends StatelessWidget {
               children: [
                 for (int i = 0; i < 5; i++)
                   Icon(
-                    i < rating.floor()
+                    i < rating!.floor()
                         ? CupertinoIcons.star_fill
                         : CupertinoIcons.star,
                     color: AppColors.amberColor,
@@ -106,7 +174,7 @@ class ListProductWidget extends StatelessWidget {
               width: AppDimensions.marginSmall / 2,
             ),
             Text(
-              '(33)',
+              '($rateTotal)',
               style: Theme.of(context).textTheme.bodyText1,
             ),
           ],
@@ -114,44 +182,25 @@ class ListProductWidget extends StatelessWidget {
       ),
     );
   }
+}
 
-  Padding titleBlock(BuildContext context) {
+class ListProductTitleBlock extends StatelessWidget {
+  final String title;
+
+  const ListProductTitleBlock({Key? key, required this.title})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(
           horizontal: AppDimensions.paddingSmall,
           vertical: AppDimensions.paddingSmall / 2),
       child: Text(
-        'Смартфон Samsung Galaxy Note 10+ 12/256GB White(SM-N975FZWDSER)',
+        title,
         style: Theme.of(context).textTheme.bodyText1!.copyWith(fontSize: 16.0),
         maxLines: 3,
         softWrap: true,
         overflow: TextOverflow.ellipsis,
-      ),
-    );
-  }
-
-  Padding priceBlock(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: AppDimensions.paddingSmall,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            '13732 TMT',
-            textAlign: TextAlign.left,
-            style: Theme.of(context).textTheme.subtitle1!.copyWith(
-                color: AppColors.darkBlueColor, fontWeight: FontWeight.bold),
-          ),
-          Visibility(
-            child: Text('13732 TMT',
-                style: Theme.of(context)
-                    .textTheme
-                    .caption!
-                    .copyWith(decoration: TextDecoration.lineThrough)),
-          )
-        ],
       ),
     );
   }
