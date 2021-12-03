14be71df8045a59ef4d1e6bbe45c12729b39343a
Atamyrat Babayev <atashka_babaev@mail.ru>
2021-12-03 19:27:29 +0500

    added empty screens

---
+++ lib/screen/wishlist/wishlist_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class WishlistScreen extends StatelessWidget {
+  const WishlistScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: const Text('Wishlist screen'),
+    );
+  }
+}
