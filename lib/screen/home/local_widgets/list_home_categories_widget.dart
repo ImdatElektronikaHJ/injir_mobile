07175505831af500a12651d0f035652ca02b1392
purpurni <sekisPurp@gmail.com>
2021-12-03 14:03:00 +0500

    home categories moved to another file

--- lib/screen/home/local_widgets/list_home_categories_widget.dart
+++ lib/screen/home/local_widgets/list_home_categories_widget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tajir/theme/app_colors.dart';
+import 'package:tajir/screen/home/local_widgets/home_categories_widget.dart';
 import 'package:tajir/theme/app_dimensions.dart';
 
 class ListHomeCategoriesWidget extends StatelessWidget {
@@ -18,42 +18,6 @@ class ListHomeCategoriesWidget extends StatelessWidget {
           return const HomeCategoriesWidget();
         },
       ),
-    );
-  }
-}
-
-class HomeCategoriesWidget extends StatelessWidget {
-  const HomeCategoriesWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: Container(
-            width: AppDimensions.homeCategoriesWidth,
-            margin: const EdgeInsets.symmetric(
-              horizontal: AppDimensions.marginSmall,
-              vertical: AppDimensions.marginSmall / 2,
-            ),
-            decoration: BoxDecoration(
-                color: AppColors.lightGreyColor,
-                boxShadow: [
-                  BoxShadow(
-                      color: AppColors.lightGreyColor.withOpacity(0.2),
-                      offset: const Offset(0, 2),
-                      blurRadius: 20)
-                ],
-                borderRadius:
-                    BorderRadius.circular(AppDimensions.borderRadiusMedium)),
-          ),
-        ),
-        Text('Jeans',
-            style: Theme.of(context)
-                .textTheme
-                .bodyText1!
-                .copyWith(fontSize: 16.0)),
-      ],
     );
   }
 }
