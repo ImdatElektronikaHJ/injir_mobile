a23cf09531161c34fefb0bf11048ee833f120ff8
Atamyrat Babayev <atashka_babaev@mail.ru>
2021-12-03 19:29:12 +0500

    added dashboard navigation indexes

---
+++ lib/screen/dashboard/consts/dashboard_navigation_index.dart
@@ -0,0 +1,13 @@
+class DashboardNavigationIndex {
+  DashboardNavigationIndex._();
+
+  static const int home = 0;
+  static const int categories = 1;
+  static const int cart = 2;
+  static const int stores = 3;
+  static const int profile = 4;
+  static const int wishlist = 5;
+  static const int compare = 6;
+  static const int search = 7;
+  static const int becomeSeller = 8;
+}
