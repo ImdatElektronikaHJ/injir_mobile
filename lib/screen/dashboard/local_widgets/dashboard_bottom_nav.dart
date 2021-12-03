690a276e838565b941daa6c08c7a0889d196440c
Atamyrat Babayev <atashka_babaev@mail.ru>
2021-12-03 19:33:50 +0500

    native bottom_nav_bar replaced with custom nav bar

--- lib/screen/dashboard/local_widgets/dashboard_bottom_nav.dart
+++ lib/screen/dashboard/local_widgets/dashboard_bottom_nav.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_utils/src/extensions/internacionalization.dart';
+import 'package:tajir/screen/dashboard/consts/dashboard_navigation_index.dart';
+import 'package:tajir/screen/dashboard/local_widgets/dashboard_bottom_nav_bar.dart';
+import 'package:tajir/screen/dashboard/local_widgets/dashboard_bottom_nav_item.dart';
 
 class DashboardBottomNav extends StatelessWidget {
   final Function(int)? onItemSelected;
@@ -11,27 +14,46 @@ class DashboardBottomNav extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      items: [
-        BottomNavigationBarItem(
-            icon: const Icon(CupertinoIcons.house), label: 'home'.tr),
-        BottomNavigationBarItem(
-            icon: const Icon(CupertinoIcons.square_grid_2x2),
-            label: 'categories'.tr),
-        BottomNavigationBarItem(
-            icon: const Icon(CupertinoIcons.bag), label: 'cart'.tr),
-        BottomNavigationBarItem(
-            icon: const Icon(CupertinoIcons.house_alt), label: 'stores'.tr),
-        BottomNavigationBarItem(
-            icon: const Icon(CupertinoIcons.person), label: 'profile'.tr),
-      ],
-      unselectedFontSize: 0.0,
-      selectedFontSize: 0.0,
-      showSelectedLabels: false,
-      showUnselectedLabels: false,
-      type: BottomNavigationBarType.fixed,
-      onTap: onItemSelected,
-      currentIndex: currentIndex,
-    );
+    return DashboardBottomNavBar(items: [
+      DashboardBottomNavItem(
+        value: DashboardNavigationIndex.home,
+        groupValue: currentIndex,
+        iconData: CupertinoIcons.house,
+        tooltip: 'home'.tr,
+        onTapped: () => _onItemTapped(DashboardNavigationIndex.home),
+      ),
+      DashboardBottomNavItem(
+        value: DashboardNavigationIndex.categories,
+        groupValue: currentIndex,
+        iconData: CupertinoIcons.square_grid_2x2,
+        tooltip: 'categories'.tr,
+        onTapped: () => _onItemTapped(DashboardNavigationIndex.categories),
+      ),
+      DashboardBottomNavItem(
+        value: DashboardNavigationIndex.cart,
+        groupValue: currentIndex,
+        iconData: CupertinoIcons.bag,
+        tooltip: 'cart'.tr,
+        onTapped: () => _onItemTapped(DashboardNavigationIndex.cart),
+      ),
+      DashboardBottomNavItem(
+        value: DashboardNavigationIndex.stores,
+        groupValue: currentIndex,
+        iconData: CupertinoIcons.house_alt,
+        tooltip: 'stores'.tr,
+        onTapped: () => _onItemTapped(DashboardNavigationIndex.stores),
+      ),
+      DashboardBottomNavItem(
+        value: DashboardNavigationIndex.profile,
+        groupValue: currentIndex,
+        iconData: CupertinoIcons.person,
+        tooltip: 'profile'.tr,
+        onTapped: () => _onItemTapped(DashboardNavigationIndex.profile),
+      ),
+    ]);
+  }
+
+  _onItemTapped(int index) {
+    onItemSelected!(index);
   }
 }
