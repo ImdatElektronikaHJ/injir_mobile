85ea9dda2f9ca1f489a18fef3cb49e91a6836a4e
Mekan1206 <myradow207@gmail.com>
2021-12-06 15:21:00 +0500

    splash screen renamed

--- lib/screen/splash/splash_screen.dart
+++ lib/screen/splash/splash_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'local_widget/splash_screen_widget.dart';
+import 'local_widgets/splash_screen_widget.dart';
 
 // ignore: use_key_in_widget_constructors
-class MyHomePage extends StatelessWidget {
+class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
