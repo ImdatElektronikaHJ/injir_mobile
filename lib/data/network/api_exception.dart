6e4030ca01e7c5ade714ddfabc3b2f9a3b8256df
Atamyrat Babayev <atashka_babaev@mail.ru>
2021-12-08 20:30:48 +0500

    added api exception

---
+++ lib/data/network/api_exception.dart
@@ -0,0 +1,38 @@
+class ApiException implements Exception {
+  final String? _message;
+  final String? _prefix;
+
+  ApiException([this._message, this._prefix]);
+
+  @override
+  String toString() {
+    return "$_prefix$_message";
+  }
+}
+
+class FetchDataException extends ApiException {
+  FetchDataException([String? message])
+      : super(message, "Error During Communication: ");
+}
+
+class BadRequestException extends ApiException {
+  BadRequestException([String? message]) : super(message, "Invalid Request: ");
+}
+
+class HeaderMalformedException extends ApiException {
+  HeaderMalformedException([String? message])
+      : super(message, "Header Malformed: ");
+}
+
+class UnauthorisedException extends ApiException {
+  UnauthorisedException([String? message]) : super(message, "Unauthorised: ");
+}
+
+class InvalidInputException extends ApiException {
+  InvalidInputException([String? message]) : super(message, "Invalid Input: ");
+}
+
+class AuthenticationException extends ApiException {
+  AuthenticationException([String? message])
+      : super(message, "Authentication Failed: ");
+}
