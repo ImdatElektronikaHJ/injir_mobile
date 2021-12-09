45b79428fca305e9ac9be881a69d8fe95fb408c0
purpurni <sekisPurp@gmail.com>
2021-12-09 21:35:00 +0500

    created sale widget

---
+++ lib/widget/sale_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:tajir/theme/app_colors.dart';
+import 'package:tajir/theme/app_dimensions.dart';
+
+class SaleWidget extends StatelessWidget {
+  final String? salePercentage;
+
+  const SaleWidget({Key? key, this.salePercentage}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin:
+          const EdgeInsets.symmetric(horizontal: AppDimensions.paddingSmall),
+      decoration: BoxDecoration(
+        color: AppColors.redColor,
+        borderRadius:
+            BorderRadius.circular(AppDimensions.borderRadiusSmall / 2),
+      ),
+      padding: const EdgeInsets.symmetric(
+          horizontal: AppDimensions.paddingSmall / 1.5,
+          vertical: AppDimensions.paddingSmall / 3),
+      child: Text(
+        '-$salePercentage%',
+        maxLines: 1,
+        style: Theme.of(context)
+            .textTheme
+            .button!
+            .copyWith(color: AppColors.whiteColor, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
