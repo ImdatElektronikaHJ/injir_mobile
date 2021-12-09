9c8d9ad6de4f6135d1e99cde5e36a84ac0c6c3fc
purpurni <sekisPurp@gmail.com>
2021-12-09 21:40:37 +0500

    created product description

---
+++ lib/screen/product/local_widgets/product_description.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductDescription extends StatelessWidget {
+  final String description;
+  const ProductDescription({Key? key, required this.description})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      description,
+      style: Theme.of(context).textTheme.subtitle1,
+    );
+  }
+}
