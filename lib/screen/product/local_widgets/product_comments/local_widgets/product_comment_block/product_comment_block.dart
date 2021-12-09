48f9d629990cdf5c3fb2425a64b4e11cb5abb02f
purpurni <sekisPurp@gmail.com>
2021-12-09 21:38:37 +0500

    created product comment block

---
+++ lib/screen/product/local_widgets/product_comments/local_widgets/product_comment_block/product_comment_block.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:tajir/theme/app_dimensions.dart';
+
+import 'local_widgets/product_comment_block_rating.dart';
+import 'local_widgets/product_comments_block_date.dart';
+
+class ProductCommentBlock extends StatelessWidget {
+  final String title;
+  final double rating;
+  final String description;
+  final String date;
+
+  const ProductCommentBlock(
+      {Key? key,
+      required this.title,
+      required this.rating,
+      required this.description,
+      required this.date})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: AppDimensions.marginMedium),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(title,
+              style: Theme.of(context)
+                  .textTheme
+                  .subtitle1!
+                  .copyWith(fontSize: 17.0)),
+          const ProductCommentRating(rating: 4.9),
+          Text(
+            description,
+            style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                  fontSize: 16.0,
+                ),
+          ),
+          ProductCommentBlockDate(date: date),
+        ],
+      ),
+    );
+  }
+}
