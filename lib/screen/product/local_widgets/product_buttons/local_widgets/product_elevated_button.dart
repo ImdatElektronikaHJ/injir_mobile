e86bb02c4a156017bee8353318226847141a6e47
purpurni <sekisPurp@gmail.com>
2021-12-09 21:39:23 +0500

    created product elevated button

---
+++ lib/screen/product/local_widgets/product_buttons/local_widgets/product_elevated_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:tajir/theme/app_dimensions.dart';
+
+class ProductElevatedButton extends StatelessWidget {
+  final String title;
+  final Function onTapped;
+  final Color? buttonColor;
+
+  const ProductElevatedButton(
+      {Key? key, required this.title, required this.onTapped, this.buttonColor})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => onTapped(),
+      style: ElevatedButton.styleFrom(
+        primary: buttonColor,
+        minimumSize: Size(MediaQuery.of(context).size.width,
+            AppDimensions.productElevatedButtonHeight),
+        elevation: 0.0,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(
+              AppDimensions.borderRadiusSmall,
+            ),
+          ),
+        ),
+      ),
+      child: Text(
+        title,
+        style: Theme.of(context)
+            .textTheme
+            .button!
+            .copyWith(fontSize: 18.0, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
