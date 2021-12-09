f445556743663181a01b8605b796317e9563317e
purpurni <sekisPurp@gmail.com>
2021-12-09 21:38:17 +0500

    created product comments block date

---
+++ lib/screen/product/local_widgets/product_comments/local_widgets/product_comment_block/local_widgets/product_comments_block_date.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tajir/theme/app_dimensions.dart';
+
+class ProductCommentBlockDate extends StatelessWidget {
+  final String date;
+
+  const ProductCommentBlockDate({Key? key, required this.date})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Icon(
+          CupertinoIcons.clock,
+          color: Colors.grey,
+          size: 18.0,
+        ),
+        const SizedBox(
+          width: AppDimensions.paddingSmall / 2,
+        ),
+        Text(
+          date,
+          style: Theme.of(context).textTheme.caption,
+        ),
+      ],
+    );
+  }
+}
