ad5ea8e48b8ad656aa74f5adf55b91ce3eb8710d
purpurni <sekisPurp@gmail.com>
2021-12-09 21:37:16 +0500

    created product specification with title

---
+++ lib/screen/product/local_widgets/product_specifications/local_widgets/product_spetification_with_title.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:tajir/screen/product/local_widgets/product_specifications/local_widgets/product_spetification.dart';
+import 'package:tajir/theme/app_colors.dart';
+import 'package:tajir/theme/app_dimensions.dart';
+
+class ProductSpecificationWithTitle extends StatelessWidget {
+  final String title;
+  final List<Map<String, String>> specifications;
+  const ProductSpecificationWithTitle(
+      {Key? key, required this.specifications, required this.title})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding:
+              const EdgeInsets.symmetric(vertical: AppDimensions.paddingSmall),
+          child: Text(
+            title,
+            style: Theme.of(context)
+                .textTheme
+                .bodyText1!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+        ),
+        for (int i = 0; i < specifications.length; i++)
+          ProductSpetification(
+            title: specifications[i]['title']!,
+            description: specifications[i]['spec']!,
+            primaryColor: i % 2 != 0
+                ? AppColors.whiteColor
+                : AppColors.lightBlueGreyColor,
+          ),
+      ],
+    );
+  }
+}
