e0f2b4eb624360e3c83ebf2d5d41b7b96c06c75c
Mekan1206 <myradow207@gmail.com>
2021-12-14 16:47:37 +0500

    update svg icons file

--- lib/theme/svg_icons.dart
+++ lib/theme/svg_icons.dart
@@ -1,8 +1,8 @@
 class SvgImages {
-  static String login = "assets/images/svg/APPBAR-TOP.svg";
-  static String person = "assets/images/svg/Group6.svg";
-  static String arrowright = "assets/images/svg/ic-chevron.svg";
-  static String local = "assets/images/svg/Group3.svg";
-  static String clock = "assets/images/svg/Group7.svg";
-  static String lang = "assets/images/svg/Group9.svg";
+  static String login = "assets/images/svg/login.svg";
+  static String person = "assets/images/svg/person.svg";
+  static String arrowright = "assets/images/svg/arrow_right.svg";
+  static String local = "assets/images/svg/local.svg";
+  static String clock = "assets/images/svg/clock.svg";
+  static String lang = "assets/images/svg/lang.svg";
 }
