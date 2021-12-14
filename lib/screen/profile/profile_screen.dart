869f553e0209327fe64ca843929ec88588acb621
Mekan1206 <myradow207@gmail.com>
2021-12-14 16:47:10 +0500

    update profile screen

--- lib/screen/profile/profile_screen.dart
+++ lib/screen/profile/profile_screen.dart
@@ -8,73 +8,70 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: CustomScrollView(slivers: [
-        const SliverToBoxAdapter(
-          child: SizedBox(
-            height: AppDimension.marginMedium,
-          ),
+    return CustomScrollView(slivers: [
+      const SliverToBoxAdapter(
+        child: SizedBox(
+          height: AppDimension.marginMedium,
         ),
-        SliverToBoxAdapter(
-          child: Column(
-            children: [
-              ListTile(
-                leading: Text(
-                  'Profile',
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline6!
-                      .copyWith(fontWeight: FontWeight.w600),
-                ),
-                trailing: SvgPicture.asset(
-                  SvgImages.login,
-                ),
+      ),
+      SliverToBoxAdapter(
+        child: Column(
+          children: [
+            ListTile(
+              leading: Text(
+                'Profile',
+                style: Theme.of(context)
+                    .textTheme
+                    .headline6!
+                    .copyWith(fontWeight: FontWeight.w600),
               ),
-              ListTile(
-                leading: SvgPicture.asset(
-                  SvgImages.person,
-                ),
-                title: Text('Edit Profile',
-                    style: Theme.of(context).textTheme.subtitle2!),
-                trailing: SvgPicture.asset(
-                  SvgImages.arrowright,
-                ),
+              trailing: SvgPicture.asset(
+                SvgImages.login,
               ),
-              ListTile(
-                leading: SvgPicture.asset(
-                  SvgImages.local,
-                ),
-                title: Text('Address',
-                    style: Theme.of(context).textTheme.subtitle2!),
-                trailing: SvgPicture.asset(
-                  SvgImages.arrowright,
-                ),
+            ),
+            ListTile(
+              leading: SvgPicture.asset(
+                SvgImages.person,
               ),
-              ListTile(
-                leading: SvgPicture.asset(
-                  SvgImages.clock,
-                ),
-                title: Text('Order History',
-                    style: Theme.of(context).textTheme.subtitle2!),
-                trailing: SvgPicture.asset(
-                  SvgImages.arrowright,
-                ),
+              title: Text('Edit Profile',
+                  style: Theme.of(context).textTheme.subtitle2!),
+              trailing: SvgPicture.asset(
+                SvgImages.arrowright,
               ),
-              ListTile(
-                leading: SvgPicture.asset(
-                  SvgImages.lang,
-                ),
-                title: Text('Language',
-                    style: Theme.of(context).textTheme.subtitle2!),
-                trailing: SvgPicture.asset(
-                  SvgImages.arrowright,
-                ),
+            ),
+            ListTile(
+              leading: SvgPicture.asset(
+                SvgImages.local,
               ),
-            ],
-          ),
+              title: Text('Address',
+                  style: Theme.of(context).textTheme.subtitle2!),
+              trailing: SvgPicture.asset(
+                SvgImages.arrowright,
+              ),
+            ),
+            ListTile(
+              leading: SvgPicture.asset(
+                SvgImages.clock,
+              ),
+              title: Text('Order History',
+                  style: Theme.of(context).textTheme.subtitle2!),
+              trailing: SvgPicture.asset(
+                SvgImages.arrowright,
+              ),
+            ),
+            ListTile(
+              leading: SvgPicture.asset(
+                SvgImages.lang,
+              ),
+              title: Text('Language',
+                  style: Theme.of(context).textTheme.subtitle2!),
+              trailing: SvgPicture.asset(
+                SvgImages.arrowright,
+              ),
+            ),
+          ],
         ),
-      ]),
-    );
+      ),
+    ]);
   }
 }
