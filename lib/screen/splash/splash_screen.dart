1084c2ddce9927d7665896c4f4db3ae4215ca25d
Atamyrat Babayev <atashka_babaev@mail.ru>
2021-12-16 12:41:14 +0500

    added is first open logic and annotatedregion

--- lib/screen/splash/splash_screen.dart
+++ lib/screen/splash/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:tajir/const/app_routes.dart';
+import 'package:tajir/data/shared_preferences/storage_initial.dart';
 import 'package:tajir/theme/app_animation.dart';
 import 'package:tajir/theme/app_colors.dart';
 import 'local_widgets/blue_painter.dart';
@@ -20,11 +21,6 @@ class _SplashScreenState extends State<SplashScreen>
 
   @override
   void initState() {
-    SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(
-        systemNavigationBarColor: AppColors.darkBlueColor,
-      ),
-    );
     _controller = AnimationController(
       duration:
           const Duration(milliseconds: AppAnimation.splashAnimationDuration),
@@ -36,30 +32,33 @@ class _SplashScreenState extends State<SplashScreen>
     );
     _controller.addListener(_controllerListener);
     _controller.forward();
-
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(),
-      backgroundColor: Theme.of(context).primaryColor,
-      body: CustomPaint(
-        willChange: true,
-        painter: BluePainter(animation: _controller),
-        child: Center(
-          child: AnimatedBuilder(
-              animation: _controller,
-              builder: (context, _) {
-                return FadeTransition(
-                  opacity: _fadeInAnimation,
-                  child: Image.asset(
-                    'assets/images/tajir_logo_text.png',
-                  ),
-                );
-              }),
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarIconBrightness: Brightness.light,
+          systemNavigationBarColor: AppColors.darkBlueColor),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: CustomPaint(
+          willChange: true,
+          painter: BluePainter(animation: _controller),
+          child: Center(
+            child: AnimatedBuilder(
+                animation: _controller,
+                builder: (context, _) {
+                  return FadeTransition(
+                    opacity: _fadeInAnimation,
+                    child: Image.asset(
+                      'assets/images/tajir_logo_text.png',
+                    ),
+                  );
+                }),
+          ),
         ),
       ),
     );
@@ -67,18 +66,21 @@ class _SplashScreenState extends State<SplashScreen>
 
   _controllerListener() {
     if (_controller.value == 1.0) {
-      //TODO: Add sync logic here, will use open dashboard screen for now
-      Get.offAndToNamed(AppRoutes.dashboardRoute);
+      _openNextScreen();
+    }
+  }
+
+  _openNextScreen() async {
+    if (await StorageInitial.isFirstOpen()) {
+      await StorageInitial.updateFirstOpen();
+      await Get.offAndToNamed(AppRoutes.showcaseRoute);
+    } else {
+      await Get.offAndToNamed(AppRoutes.dashboardRoute);
     }
   }
 
   @override
   void dispose() {
-    SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(
-        systemNavigationBarColor: Colors.white,
-      ),
-    );
     _controller.dispose();
     super.dispose();
   }
