45698b8f636003a5450721df1d8c5f769bf3adf9
Mekan1206 <myradow207@gmail.com>
2021-12-21 17:14:19 +0500

    added stores screen app bar

---
+++ lib/screen/stores/local_widgets/stores_app_bar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:tajir/theme/app_dimension.dart';
+import 'package:tajir/theme/svg_icons.dart';
+
+class StoresAppBar extends StatelessWidget {
+  const StoresAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+          backgroundColor: Colors.white,
+          centerTitle: false,
+          title:
+              Text('Магазины', style: Theme.of(context).textTheme.headline6!),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(AppDimension.paddingSmall),
+              child: SvgPicture.asset(SvgImages.filter),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(AppDimension.paddingSmall),
+              child: SvgPicture.asset(SvgImages.katalog),
+            ),
+          ],
+          floating: true,
+          pinned: true,
+        );
+  }
+}
