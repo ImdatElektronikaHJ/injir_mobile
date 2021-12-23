ccfa5bb000cbb1fc2d919b85e2ac45790b7df028
Mekan1206 <myradow207@gmail.com>
2021-12-23 16:49:30 +0500

    updated categories screen

--- lib/screen/categories/categories_screen.dart
+++ lib/screen/categories/categories_screen.dart
@@ -2,22 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tajir/const/app_routes.dart';
 import 'package:tajir/const/nested_navigation_ids.dart';
-import 'package:tajir/screen/categories/local_widgets/category_item_widget.dart';
+
+
+import 'local_widgets/categories_app_bar.dart';
+import 'local_widgets/categories_screen_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: 8,
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 1.2,
-      ),
-      itemBuilder: (context, index) {
-        return CategoryItemWidget( onCategoryTapped: _goToCategoryScreen,);
-      },
+    return CustomScrollView(
+      slivers: [
+        const CategoriesAppBar(),
+        SliverGrid(
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 1.2,
+          ),
+          delegate:
+              SliverChildBuilderDelegate((BuildContext context, int index) {
+            return CategoriesScreenItem( onCategoryTapped: _goToCategoryScreen,);
+          },childCount: 8),
+        ),
+      ],
     );
   }
 
