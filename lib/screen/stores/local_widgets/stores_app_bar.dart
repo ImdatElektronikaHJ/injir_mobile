79e4be352f96e5e4fef7e2c50c83e07d65cf5c97
Mekan1206 <myradow207@gmail.com>
2021-12-23 16:50:42 +0500

    updated stores app bar

--- lib/screen/stores/local_widgets/stores_app_bar.dart
+++ lib/screen/stores/local_widgets/stores_app_bar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:tajir/theme/app_dimension.dart';
 import 'package:tajir/theme/svg_icons.dart';
+import 'package:get/get_utils/src/extensions/internacionalization.dart';
 
 class StoresAppBar extends StatelessWidget {
   const StoresAppBar({Key? key}) : super(key: key);
@@ -9,10 +10,10 @@ class StoresAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(
-          backgroundColor: Colors.white,
+          backgroundColor: Theme.of(context).backgroundColor,
           centerTitle: false,
           title:
-              Text('Магазины', style: Theme.of(context).textTheme.headline6!),
+              Text('stores'.tr, style: Theme.of(context).textTheme.headline6!),
           actions: [
             Padding(
               padding: const EdgeInsets.all(AppDimension.paddingSmall),
@@ -24,7 +25,6 @@ class StoresAppBar extends StatelessWidget {
             ),
           ],
           floating: true,
-          pinned: true,
         );
   }
 }
