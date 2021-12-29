c1422e361186292ac84e581ad48a1c8efa0ace6a
Atamyrat Babayev <atashka_babaev@mail.ru>
2021-12-29 12:00:06 +0500

    updated search screen

--- lib/screen/search/search_screen.dart
+++ lib/screen/search/search_screen.dart
@@ -1,16 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:tajir/screen/search/local_widgets/search_header_widget.dart';
+import 'package:tajir/screen/search/local_widgets/search_list_product.dart';
+import 'package:tajir/screen/search/local_widgets/search_tag_widget.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: GestureDetector(
-        onTap: _goToProductScreen,
-        child: const Text('Search screen'),
-      ),
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SearchHeader(
+                title: 'Часто задаваемые'.toUpperCase(),
+              ),
+              Wrap(
+                children: const [
+                  SearchTagWidget(
+                    tagTitle: 'IphoneX',
+                  ),
+                  SearchTagWidget(
+                    tagTitle: 'Xiaomi',
+                  ),
+                  SearchTagWidget(
+                    tagTitle: 'Huawei',
+                  ),
+                  SearchTagWidget(
+                    tagTitle: 'Nokia',
+                  ),
+                  SearchTagWidget(
+                    tagTitle: 'Samsung',
+                  ),
+                  SearchTagWidget(
+                    tagTitle: 'T-shirt',
+                  ),
+                ],
+              ),
+              SearchHeader(
+                title: 'Рекомендованные'.toUpperCase(),
+              ),
+            ],
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate(
+            [
+              SearchListProduct(),
+              SearchListProduct(),
+              SearchListProduct(),
+              SearchListProduct(),
+            ],
+          ),
+        ),
+      ],
     );
   }
 
