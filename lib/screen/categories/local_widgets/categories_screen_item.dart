34221e0d2de8ca931199f25103fbad9bc2ecb68e
Atamyrat Babayev <atashka_babaev@mail.ru>
2021-12-29 16:36:23 +0500

    updated categories item widget

--- lib/screen/categories/local_widgets/categories_screen_item.dart
+++ lib/screen/categories/local_widgets/categories_screen_item.dart
@@ -1,57 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:tajir/theme/app_colors.dart';
 import 'package:tajir/theme/app_dimension.dart';
 
 class CategoriesScreenItem extends StatelessWidget {
   final Function? onCategoryTapped;
 
-  const CategoriesScreenItem({Key? key,this.onCategoryTapped})   : super(key: key);
+  const CategoriesScreenItem({Key? key, this.onCategoryTapped})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-          child: Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: AppDimension.paddingMedium, horizontal: AppDimension.paddingSmall,),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.03),
-                    spreadRadius: 0,
-                    blurRadius: 5,
-                    offset: const Offset(0, 0),
-                  ),
-                ],
+      child: Container(
+        padding: const EdgeInsets.all(AppDimension.paddingSmall),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: const [
+              BoxShadow(
+                color: AppColors.blackColor6,
+                blurRadius: 19,
+                offset: Offset(0, 2),
               ),
-              child: Padding(
-                padding: const EdgeInsets.only(top: AppDimension.paddingSmall),
-                child: Column(children: [
-                  Expanded(
-                    flex: 2,
-                    child: Container(
-                      decoration:const  BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage('assets/images/placeholder/1t.png'),
-                            fit: BoxFit.contain),
-                      ),
-                    ),
-                  ),
-                  const Expanded(
-                    child: Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Text(
-                            'Элетроника',
-                            // overflow: TextOverflow.ellipsis,
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                  ),
-                ]),
+            ],
+            borderRadius:
+                BorderRadius.circular(AppDimension.borderRadiusSmall)),
+        child: Column(children: [
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/placeholder/1t.png'),
+                    fit: BoxFit.contain),
               ),
             ),
           ),
-          onTap: () => onCategoryTapped!(),
-        );
+          Text(
+            'Элетроника',
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+        ]),
+      ),
+      onTap: () => onCategoryTapped!(),
+    );
   }
 }
