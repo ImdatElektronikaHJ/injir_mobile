e6a9b02bfe46512d5b78a2a62047f274c8d8ab92
Atamyrat Babayev <atashka_babaev@mail.ru>
2022-01-03 20:54:04 +0500

    updated cart screen

--- lib/screen/cart/cart_screen.dart
+++ lib/screen/cart/cart_screen.dart
@@ -1,13 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:tajir/screen/cart/local_widgets/cart_app_bar.dart';
+import 'package:tajir/screen/cart/local_widgets/cart_product_item.dart';
+import 'package:tajir/screen/cart/local_widgets/cart_total_bar.dart';
+import 'package:tajir/theme/app_dimension.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: const Text('Cart screen'),
-    );
+    return Column(children: [
+      Expanded(
+        child: CustomScrollView(
+          slivers: [
+            const CartAppBar(),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: AppDimension.paddingMedium),
+                child: Text(
+                  '5 товаров',
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+              ),
+            ),
+            const SliverList(
+              delegate: SliverChildListDelegate.fixed(
+                [
+                  CartProductItem(
+                    isShadowVisible: false,
+                  ),
+                  CartProductItem(),
+                  CartProductItem(
+                    isShadowVisible: false,
+                  ),
+                  CartProductItem(),
+                  CartProductItem(
+                    isShadowVisible: false,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      const CartTotalBar(),
+    ]);
   }
 }
