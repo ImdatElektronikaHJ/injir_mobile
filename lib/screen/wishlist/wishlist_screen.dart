3816e38727ed8020e6f99279c86adf761594fefc
Atamyrat Babayev <atashka_babaev@mail.ru>
2022-01-03 21:42:31 +0500

    updated wishlist ui

--- lib/screen/wishlist/wishlist_screen.dart
+++ lib/screen/wishlist/wishlist_screen.dart
@@ -1,13 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:tajir/screen/wishlist/local_widgets/wishlist_app_bar.dart';
+import 'package:tajir/theme/app_dimension.dart';
+
+import 'local_widgets/wishlist_product_item.dart';
 
 class WishlistScreen extends StatelessWidget {
-  const WishlistScreen({ Key? key }) : super(key: key);
+  const WishlistScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: const Text('Wishlist screen'),
+    return CustomScrollView(
+      slivers: [
+        const WishlistAppBar(),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+                horizontal: AppDimension.paddingMedium),
+            child: Text(
+              '5 товаров',
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+          ),
+        ),
+        const SliverList(
+          delegate: SliverChildListDelegate.fixed(
+            [
+              WishlistProductItem(
+                isShadowVisible: false,
+              ),
+              WishlistProductItem(),
+              WishlistProductItem(
+                isShadowVisible: false,
+              ),
+              WishlistProductItem(),
+              WishlistProductItem(
+                isShadowVisible: false,
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
