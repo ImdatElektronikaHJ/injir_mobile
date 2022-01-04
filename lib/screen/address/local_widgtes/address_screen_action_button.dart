ce0c5dd84a4f5614cb1eda967638f082acf92281
Mekan1206 <myradow207@gmail.com>
2022-01-04 21:10:26 +0500

    added address screen action button

---
+++ lib/screen/address/local_widgtes/address_screen_action_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:tajir/theme/app_dimension.dart';
+
+class AddressScreenActionButton extends StatelessWidget {
+  final double padding;
+  final String title;
+  final Function()? onActionTapped;
+  
+  const AddressScreenActionButton({Key? key,
+      this.padding = AppDimension.paddingMedium,
+      required this.title,
+      this.onActionTapped})
+      : super(key: key);
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+          horizontal: padding, vertical: AppDimension.paddingSmall),
+      child: ElevatedButton(onPressed: onActionTapped, child: Text(title)),
+    );
+  }
+}
