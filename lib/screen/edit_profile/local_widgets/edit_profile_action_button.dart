1d76742d36f160cbf6f2f8679e47b55fa871e1fb
Mekan1206 <myradow207@gmail.com>
2022-01-09 14:11:22 +0500

    updated edit profile action button

--- lib/screen/edit_profile/local_widgets/edit_profile_action_button.dart
+++ lib/screen/edit_profile/local_widgets/edit_profile_action_button.dart
@@ -2,21 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:tajir/theme/app_dimension.dart';
 
 class EditProfileActionButton extends StatelessWidget {
-  final double padding;
   final String title;
   final Function()? onActionTapped;
 
   const EditProfileActionButton(
       {Key? key,
-      this.padding = AppDimension.paddingMedium,
       required this.title,
       this.onActionTapped})
       : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(
-          horizontal: padding, vertical: AppDimension.paddingSmall),
+      padding: const EdgeInsets.symmetric(
+          horizontal: AppDimension.paddingMedium, vertical: AppDimension.paddingSmall),
       child: ElevatedButton(onPressed: onActionTapped, child: Text(title)),
     );
   }
