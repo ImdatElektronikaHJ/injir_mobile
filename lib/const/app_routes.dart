47ca0631d323973713cc84d464e7fed8e39686ba
Mekan1206 <myradow207@gmail.com>
2022-01-09 14:14:19 +0500

    updated app routes

--- lib/const/app_routes.dart
+++ lib/const/app_routes.dart
@@ -8,7 +8,7 @@ class AppRoutes {
   static const String cartRoute = '/cart';
   static const String storesRoute = '/stores';
   static const String profileRoute = '/profile';
-  static const String addressRoute = '/address';
+  static const String profileAddressRoute = '/profile_address';
   static const String categoryRoute = '/category';
   static const String productRoute = '/product';
   static const String becomeSellerRoute = '/become_seller';
