c13bcd23b02768049a50922ba18648c43d24de69
purpurni <sekisPurp@gmail.com>
2022-01-19 21:54:37 +0500

    created shipping method container

---
+++ lib/screen/checkout_screen/local_widgets/shipping_method/local_widgets/shipping_method_container.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tajir/theme/app_colors.dart';
+import 'package:tajir/theme/app_dimension.dart';
+
+class ShippingMethodContainer extends StatelessWidget {
+  final bool isChecked;
+  final String title;
+  final Function onPressed;
+
+  const ShippingMethodContainer(
+      {Key? key,
+      required this.isChecked,
+      required this.title,
+      required this.onPressed})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        onPressed();
+      },
+      child: Container(
+        height: 120,
+        margin: const EdgeInsets.symmetric(
+            horizontal:
+                AppDimension.marginLarge + AppDimension.marginExtraLarge,
+            vertical: AppDimension.marginMedium / 2),
+        padding: const EdgeInsets.all(AppDimension.paddingMedium),
+        decoration: BoxDecoration(
+          color: AppColors.whiteColor,
+          borderRadius: BorderRadius.circular(AppDimension.borderRadiusMicro),
+          boxShadow: const [
+            BoxShadow(
+              color: AppColors.blackColor10,
+              blurRadius: 10.0,
+            ),
+          ],
+        ),
+        alignment: Alignment.center,
+        child: Row(
+          children: [
+            Expanded(
+              child: Text(title,
+                  style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                        fontWeight: FontWeight.bold,
+                        color: isChecked
+                            ? AppColors.blueColor
+                            : AppColors.darkGreyColor,
+                      )),
+            ),
+            AnimatedSwitcher(
+              duration: const Duration(milliseconds: 100),
+              child: Icon(
+                isChecked
+                    ? CupertinoIcons.largecircle_fill_circle
+                    : CupertinoIcons.circle,
+                key: UniqueKey(),
+                color: isChecked ? AppColors.blueColor : AppColors.greyColor,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
