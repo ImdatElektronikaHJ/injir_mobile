0ccf0cfc2c81880d61eafc0d1ab3094de6fe216a
Mekan1206 <myradow207@gmail.com>
2022-03-09 17:08:15 +0500

    Addded category screen ui

--- lib/screen/category/category_screen.dart
+++ lib/screen/category/category_screen.dart
@@ -1,13 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:tajir/screen/categories/local_widgets/categories_screen_item.dart';
+import 'package:tajir/screen/category/local_widgets/category_app_bar.dart';
+import 'package:tajir/screen/category/local_widgets/category_product.dart';
+import 'package:tajir/screen/wishlist/local_widgets/wishlist_product_item.dart';
+import 'package:tajir/theme/app_dimension.dart';
+import 'package:tajir/widget/featured_block/featured_block.dart';
+import 'package:tajir/widget/list_list_product_widget.dart';
+
+import 'local_widgets/category_screen_item.dart';
 
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: const Text('Category'),
+    return CustomScrollView(
+      slivers: [
+        const CategoryAppBar(),
+        SliverPadding(
+          padding: const EdgeInsets.symmetric(
+              vertical: AppDimension.paddingMedium,
+              horizontal: AppDimension.paddingSmall),
+          sliver: SliverGrid(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: AppDimension.paddingSmall,
+                crossAxisSpacing: AppDimension.paddingSmall),
+            delegate:
+                SliverChildBuilderDelegate((BuildContext context, int index) {
+              return const CategoryScreenItem();
+            }, childCount: 4),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (BuildContext context, int index) {
+              return const CategoryProdcut();
+            },
+            childCount: 7,
+          ),
+        ),
+        const SliverToBoxAdapter(
+          child: FeaturedBlockWidget(
+            title: 'New items',
+            childWidget: ListListProductWidget(),
+          ),
+        )
+      ],
     );
   }
 }
