def1213a06a541c182830108d439736788036ffe
Mickey-A-Mouse <atashka_babaev@mail.ru>
2022-04-29 18:40:24 +0500

    added dummy initial registration screen

---
+++ lib/screen/init_registration/init_registration_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InitRegistrationScreen extends StatelessWidget {
+  const InitRegistrationScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
