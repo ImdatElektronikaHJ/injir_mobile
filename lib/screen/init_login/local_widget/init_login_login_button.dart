11cb5c0bacf17eeedbb341a114756e488c50feeb
Mickey-A-Mouse <atashka_babaev@mail.ru>
2022-04-29 18:44:16 +0500

    added circular indicator

--- lib/screen/init_login/local_widget/init_login_login_button.dart
+++ lib/screen/init_login/local_widget/init_login_login_button.dart
@@ -1,20 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tajir/theme/app_button_style.dart';
+import 'package:tajir/theme/app_dimension.dart';
 
 class InitLoginLoginButton extends StatelessWidget {
   final void Function()? onLoginTapped;
-  const InitLoginLoginButton({Key? key, this.onLoginTapped}) : super(key: key);
+  final bool isLoading;
+  const InitLoginLoginButton(
+      {Key? key, this.onLoginTapped, this.isLoading = false})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: ElevatedButton(
-        onPressed: onLoginTapped,
-        child: Text('login_now'.tr),
-        style: AppButtonStyle.elevatedButtonStyleLarge,
-      ),
+      child: isLoading
+          ? ElevatedButton(
+              onPressed: onLoginTapped,
+              child: SizedBox(
+                height:
+                    AppDimension.fontSizeLarge + AppDimension.paddingLarge - 1,
+                width:
+                    AppDimension.fontSizeLarge + AppDimension.paddingLarge - 1,
+                child: const CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                ),
+              ),
+              style: AppButtonStyle.elevatedButtonStyleLarge.copyWith(
+                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                  const EdgeInsets.all(AppDimension.paddingMedium),
+                ),
+              ),
+            )
+          : ElevatedButton(
+              onPressed: onLoginTapped,
+              child: Text('login_now'.tr),
+              style: AppButtonStyle.elevatedButtonStyleLarge,
+            ),
     );
   }
 }
