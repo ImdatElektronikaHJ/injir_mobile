e558897f26b550432ae806141be3229d3d46dc1e
purpurni <sekisPurp@gmail.com>
2022-05-12 17:55:17 +0500

    slide banner model id & type added

--- lib/model/slide_banner.dart
+++ lib/model/slide_banner.dart
@@ -1,17 +1,19 @@
-import 'package:flutter/cupertino.dart';
-
 class SlideBanner {
+  final int? id;
   final String? title;
   final String? image;
   final String? link;
+  final String? type;
 
-  SlideBanner({this.title, @required this.image, this.link});
+  SlideBanner({this.title, required this.image, this.link, this.type, this.id});
 
   factory SlideBanner.fromJson(Map<String, dynamic> json) {
     return SlideBanner(
+      id: int.parse(json['mobile_id']),
       title: json['title'],
       link: json['link'],
       image: json['image'],
+      type: json['mobile_type'],
     );
   }
 }
