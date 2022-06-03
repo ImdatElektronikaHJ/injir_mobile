3f8effd77e366eddd59d51deecd9e520ae749ba6
purpurni <sekisPurp@gmail.com>
2022-06-03 14:57:48 +0500

    added login route

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:tajir/controller/account_controller.dart';
 import 'package:tajir/localization/localization_service.dart';
 import 'package:tajir/screen/dashboard/dashboard_screen.dart';
 import 'package:tajir/screen/init_login/init_login_screen.dart';
@@ -11,6 +12,7 @@ import 'package:tajir/theme/app_light_theme.dart';
 import 'package:tajir/theme/app_theme_service.dart';
 
 import 'const/app_routes.dart';
+import 'screen/login/login_screen.dart';
 
 class TajirApp extends StatelessWidget {
   final LocalizationService localizationService = LocalizationService();
@@ -27,12 +29,12 @@ class TajirApp extends StatelessWidget {
         translations: localizationService,
         title: 'Tajir',
         theme: AppLightTheme.appTheme,
-        //darkTheme: AppDarkTheme.theme,
         themeMode: AppThemeService().themeMode,
         getPages: [
           GetPage(
             name: AppRoutes.defaultRoute,
             page: () => const SplashScreen(),
+            binding: SplashBinding(),
           ),
           GetPage(
             name: AppRoutes.showcaseRoute,
@@ -48,6 +50,10 @@ class TajirApp extends StatelessWidget {
             page: () => RegistrationScreen(),
           ),
           GetPage(
+            name: AppRoutes.loginRoute,
+            page: () => LoginScreen(),
+          ),
+          GetPage(
             name: AppRoutes.initLoginRoute,
             page: () => InitLoginScreen(),
           ),
@@ -56,5 +62,12 @@ class TajirApp extends StatelessWidget {
             page: () => InitRegistrationScreen(),
           ),
         ]);
+  }
+}
+
+class SplashBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.put(AccountController(), permanent: true);
   }
 }
