d847768cd456aef363f12f37653602a355788a57
purpurni <sekisPurp@gmail.com>
2022-06-03 15:15:34 +0500

    added logic to edit profile screen

--- lib/screen/edit_profile/edit_profile_screen.dart
+++ lib/screen/edit_profile/edit_profile_screen.dart
@@ -1,51 +1,93 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_utils/src/extensions/internacionalization.dart';
+import 'package:tajir/controller/edit_profile_controller.dart';
 import 'package:tajir/screen/edit_profile/local_widgets/edit_profile_action_button.dart';
 import 'package:tajir/screen/edit_profile/local_widgets/edit_profile_app_bar.dart';
 import 'package:tajir/theme/app_colors.dart';
-import 'package:tajir/widget/simple_text_field_with_hint.dart';
+import 'package:tajir/theme/app_dimension.dart';
+
+import '../../base/statefull_data.dart';
+import '../../model/account.dart';
+import '../../widget/simple_text_field_with_hint.dart';
 
 class EditProfileScreen extends StatelessWidget {
-  const EditProfileScreen({Key? key}) : super(key: key);
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  EditProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.whiteColor,
-      body: CustomScrollView(
-        slivers: [
-          const EditProfileAppBar(),
-          SliverToBoxAdapter(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                // SimpleTextFieldWithHint(
-                //   fieldName: "first_name".tr,
-                // ),
-                // SimpleTextFieldWithHint(
-                //   fieldName: "last_name".tr,
-                // ),
-                // SimpleTextFieldWithHint(
-                //   fieldName: "email_address".tr,
-                //   hint: "send_confirmation".tr,
-                // ),
-                // SimpleTextFieldWithHint(
-                //   fieldName: "Date of birth".tr,
-                // ),
-                // SimpleTextFieldWithHint(
-                //   fieldName: "Gender".tr,
-                // ),
-                // SimpleTextFieldWithHint(
-                //   fieldName: "phone_number".tr,
-                // ),
-                EditProfileActionButton(
-                  onActionTapped: () {},
-                  title: 'save'.tr.toUpperCase(),
+      body: GetBuilder<EditProfileController>(
+        init: EditProfileController(formKey: _formKey),
+        builder: (controller) {
+          Account? account = controller.account;
+          return CustomScrollView(
+            slivers: [
+              const EditProfileAppBar(),
+              SliverToBoxAdapter(
+                child: Form(
+                  key: _formKey,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: AppDimension.paddingLarge),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        SimpleTextFieldWithHint(
+                          fieldName: "first_name".tr,
+                          initialValue: account.firstName,
+                          validation: (_) => controller.validateFirstName(),
+                          onValueChanged: (value) {
+                            controller.updateFirstName(value);
+                          },
+                        ),
+                        SimpleTextFieldWithHint(
+                          fieldName: "last_name".tr,
+                          initialValue: account.lastName,
+                          validation: (_) => controller.validateLastName(),
+                          onValueChanged: (value) {
+                            controller.updateLastName(value);
+                          },
+                        ),
+                        SimpleTextFieldWithHint(
+                          fieldName: "email_address".tr,
+                          hint: "send_confirmation".tr,
+                          initialValue: account.email,
+                          validation: (_) => controller.validateMail(),
+                          onValueChanged: (value) {
+                            controller.updateMail(value);
+                          },
+                        ),
+                        SimpleTextFieldWithHint(
+                          fieldName: "phone_number".tr,
+                          initialValue: account.phone,
+                          validation: (_) => controller.validatePhone(),
+                          onValueChanged: (value) {
+                            controller.updatePhone(value);
+                          },
+                        ),
+                        Padding(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: Get.width / 4),
+                          child: EditScreenSaveButton(
+                            onActionTapped: () {
+                              controller.onSaveTapped();
+                            },
+                            isLoading: controller.accountResponse.status ==
+                                Status.loading,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-              ],
-            ),
-          ),
-        ],
+              ),
+            ],
+          );
+        },
       ),
     );
   }
--- lib/screen/edit_profile/local_widgets/edit_profile_action_button.dart
+++ lib/screen/edit_profile/local_widgets/edit_profile_action_button.dart
@@ -1,21 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_utils/src/extensions/internacionalization.dart';
+import 'package:tajir/theme/app_button_style.dart';
 import 'package:tajir/theme/app_dimension.dart';
 
-class EditProfileActionButton extends StatelessWidget {
-  final String title;
+class EditScreenSaveButton extends StatelessWidget {
+  final bool isLoading;
   final Function()? onActionTapped;
 
-  const EditProfileActionButton(
-      {Key? key,
-      required this.title,
-      this.onActionTapped})
+  const EditScreenSaveButton(
+      {Key? key, this.onActionTapped, required this.isLoading})
       : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-          horizontal: AppDimension.paddingMedium, vertical: AppDimension.paddingSmall),
-      child: ElevatedButton(onPressed: onActionTapped, child: Text(title)),
+    return SizedBox(
+      width: double.infinity,
+      child: isLoading
+          ? ElevatedButton(
+              onPressed: onActionTapped,
+              child: SizedBox(
+                height:
+                    AppDimension.fontSizeLarge + AppDimension.paddingLarge - 1,
+                width:
+                    AppDimension.fontSizeLarge + AppDimension.paddingLarge - 1,
+                child: const CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                ),
+              ),
+              style: AppButtonStyle.elevatedButtonStyleLarge.copyWith(
+                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                  const EdgeInsets.all(AppDimension.paddingMedium),
+                ),
+              ),
+            )
+          : ElevatedButton(
+              onPressed: onActionTapped,
+              child: Text('save'.tr.toUpperCase()),
+              style: AppButtonStyle.elevatedButtonStyleLarge,
+            ),
     );
   }
 }
