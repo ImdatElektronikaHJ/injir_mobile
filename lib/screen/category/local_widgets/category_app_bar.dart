d7eb1e3b2eea2e55add9606996bdc14044a7518e
purpurni <sekisPurp@gmail.com>
2022-06-07 15:37:37 +0500

    added connecting with controller

--- lib/screen/category/local_widgets/category_app_bar.dart
+++ lib/screen/category/local_widgets/category_app_bar.dart
@@ -1,26 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:get/get_utils/src/extensions/internacionalization.dart';
 import 'package:tajir/theme/app_colors.dart';
-import 'package:tajir/theme/app_dimension.dart';
-import 'package:tajir/theme/svg_icons.dart';
 
 class CategoryAppBar extends StatelessWidget {
-  const CategoryAppBar({Key? key}) : super(key: key);
+  final String categoryName;
+  const CategoryAppBar({Key? key, required this.categoryName})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(
-            iconTheme: const IconThemeData(color: AppColors.blackColor),
-            backgroundColor: Theme.of(context).backgroundColor,
-            centerTitle: false,
-            title: Align(
-              alignment: Alignment.centerRight,
-              child: Text('Электроника'.tr,
-                  style: Theme.of(context).textTheme.subtitle2!),
-            ),
-            
-            floating: true,
-          );
+      iconTheme: const IconThemeData(color: AppColors.blackColor),
+      backgroundColor: Theme.of(context).backgroundColor,
+      centerTitle: false,
+      title: Align(
+        alignment: Alignment.centerRight,
+        child:
+            Text(categoryName, style: Theme.of(context).textTheme.subtitle2!),
+      ),
+      floating: true,
+    );
   }
 }
