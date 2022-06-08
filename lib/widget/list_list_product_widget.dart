9ba6ef9e97b4f0e354dc2618fd543cd2c174c3e9
purpurni <sekisPurp@gmail.com>
2022-06-08 16:41:02 +0500

    added list product to arguments

--- lib/widget/list_list_product_widget.dart
+++ lib/widget/list_list_product_widget.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:tajir/model/list_product.dart';
 import 'package:tajir/theme/app_dimension.dart';
 import 'package:tajir/widget/list_product/list_product_widget.dart';
 
 class ListListProductWidget extends StatelessWidget {
-  const ListListProductWidget({Key? key}) : super(key: key);
+  final List<ListProduct>? productList;
+  const ListListProductWidget({Key? key, this.productList}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: AppDimension.listProductHeight + AppDimension.marginExtraLarge * 2,
+      height:
+          AppDimension.listProductHeight + AppDimension.marginExtraLarge * 2,
       child: ListView.builder(
-        itemBuilder: (context, index) => const ListProductWidget(),
-        itemCount: 6,
+        itemBuilder: (context, index) => ListProductWidget(
+          listProduct: productList?[index],
+        ),
+        itemCount: productList?.length,
         scrollDirection: Axis.horizontal,
       ),
     );
