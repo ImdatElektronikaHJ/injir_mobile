e496baa95441862b8aaa3015adeb557bf199bfcd
purpurni <sekisPurp@gmail.com>
2022-06-23 17:46:01 +0500

    added logic to UI

--- lib/screen/wishlist/local_widgets/wishlist_product_item.dart
+++ lib/screen/wishlist/local_widgets/wishlist_product_item.dart
@@ -1,73 +1,124 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_utils/src/extensions/internacionalization.dart';
+import 'package:get/get.dart';
+import 'package:tajir/model/wishlist_product.dart';
 import 'package:tajir/theme/app_colors.dart';
 import 'package:tajir/theme/app_dimension.dart';
-import 'package:tajir/widget/product_counter/product_counter_widget.dart';
+import 'package:tajir/widget/caching_image.dart';
 import 'package:tajir/widget/rating_widget.dart';
 import 'package:tajir/widget/sale_widget.dart';
 
+import '../../../controller/cart_controller.dart';
+import '../../../widget/add_to_cart_widget/add_to_cart_widget.dart';
+
 class WishlistProductItem extends StatelessWidget {
   final bool isShadowVisible;
-  const WishlistProductItem({Key? key, this.isShadowVisible = true})
+  final WishlistProduct product;
+  final void Function() onRemoveTapped;
+  final void Function() onProductTapped;
+  const WishlistProductItem(
+      {Key? key,
+      this.isShadowVisible = true,
+      required this.product,
+      required this.onRemoveTapped,
+      required this.onProductTapped})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: AppDimension.paddingMedium),
-      decoration: isShadowVisible
-          ? BoxDecoration(
-              color: Theme.of(context).backgroundColor,
-              boxShadow: const [
-                BoxShadow(
-                  color: AppColors.blackColor6,
-                  blurRadius: 19,
-                  offset: Offset(0, 2),
-                ),
-              ],
-            )
-          : null,
-      height: 200.0,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(AppDimension.paddingMedium),
-            child: Image.asset('assets/images/placeholder/samsung.png'),
-          ),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                  Expanded(
-                    child: Text(
-                      'Samsung Galaxy Note 10 8/256GB Aura Black',
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyText1!
-                          .copyWith(fontSize: 16.0),
-                      maxLines: 2,
-                      softWrap: true,
-                      overflow: TextOverflow.ellipsis,
-                    ),
+    return GestureDetector(
+      onTap: () {
+        onProductTapped();
+      },
+      child: Container(
+        height: AppDimension.wishlistProductHeight,
+        padding:
+            const EdgeInsets.symmetric(vertical: AppDimension.paddingMedium),
+        decoration: isShadowVisible
+            ? BoxDecoration(
+                color: Theme.of(context).backgroundColor,
+                boxShadow: const [
+                  BoxShadow(
+                    color: AppColors.blackColor6,
+                    blurRadius: 19,
+                    offset: Offset(0, 2),
                   ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: AppDimension.paddingMedium),
-                    child: Icon(Icons.favorite, color: AppColors.redColor),
+                ],
+              )
+            : null,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            AspectRatio(
+              aspectRatio: 2 / 3,
+              child: Padding(
+                padding: const EdgeInsets.all(AppDimension.paddingMedium),
+                child: CachingImage(product.thumb),
+              ),
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Text(
+                          '${product.name}',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText1!
+                              .copyWith(fontSize: 16.0),
+                          maxLines: 2,
+                          softWrap: true,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          onRemoveTapped();
+                        },
+                        padding: const EdgeInsets.all(AppDimension.marginSmall),
+                        icon: const Icon(Icons.favorite,
+                            color: AppColors.redColor),
+                      )
+                    ],
                   ),
-                ]),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(
-                          '1259 TMT',
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Visibility(
+                        visible: product.salePrice != null,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              '${product.salePrice ?? 1} TMT',
+                              textAlign: TextAlign.left,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .subtitle1!
+                                  .copyWith(
+                                      color: AppColors.darkBlueColor,
+                                      fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              '${product.price} TMT',
+                              style:
+                                  Theme.of(context).textTheme.caption!.copyWith(
+                                        fontSize: 13.0,
+                                        decoration: TextDecoration.lineThrough,
+                                      ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Visibility(
+                        visible: product.salePrice == null,
+                        child: Text(
+                          '${product.price} TMT',
                           textAlign: TextAlign.left,
                           style: Theme.of(context)
                               .textTheme
@@ -76,56 +127,85 @@ class WishlistProductItem extends StatelessWidget {
                                   color: AppColors.darkBlueColor,
                                   fontWeight: FontWeight.bold),
                         ),
-                        Text(
-                          '1500 TMT',
+                      ),
+                      const SizedBox(
+                        width: AppDimension.paddingMedium,
+                      ),
+                      Visibility(
+                          visible: product.salePrice != null,
+                          child: SaleWidget(
+                              salePercentage: (100 -
+                                      ((product.price ?? 1) /
+                                          (product.salePrice ?? 1) *
+                                          100))
+                                  .toStringAsFixed(2))),
+                      const Expanded(child: SizedBox()),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: AppDimension.paddingMedium),
+                        child: Icon(Icons.compare_arrows,
+                            color: AppColors.blueColor),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const RatingWidget(),
+                      //TODO: no quantity in api
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: AppDimension.paddingSmall),
+                        child: Text(
+                          'in_stock'.tr,
                           style: Theme.of(context)
                               .textTheme
-                              .caption!
-                              .copyWith(decoration: TextDecoration.lineThrough),
+                              .subtitle2
+                              ?.copyWith(color: AppColors.blueColor),
                         ),
-                      ],
-                    ),
-                    const SizedBox(
-                      width: AppDimension.paddingMedium,
-                    ),
-                    const SaleWidget(salePercentage: '32'),
-                    const Expanded(child: SizedBox()),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: AppDimension.paddingMedium),
-                      child: Icon(Icons.compare_arrows,
-                          color: AppColors.blueColor),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const RatingWidget(),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: AppDimension.paddingSmall),
-                      child: Text(
-                        'В наличии',
-                        style: Theme.of(context)
-                            .textTheme
-                            .subtitle2
-                            ?.copyWith(color: AppColors.blueColor),
+                      ),
+                    ],
+                  ),
+                  GetBuilder<CartController>(
+                    builder: (cartController) => Container(
+                      alignment: Alignment.topLeft,
+                      // width: 35.0,
+                      child: AddToCartWidget(
+                        onDecrementTapped: () {
+                          cartController.decrementTapped(product.id);
+                        },
+                        onIncrementTapped: () {
+                          cartController.incrementTapped(product.id);
+                        },
+                        onAddToCartTapped: () {
+                          cartController.addProduct(
+                            productId: product.id,
+                            thumb: product.thumb,
+                            name: product.name,
+                            model: product.model,
+                            inStock: true,
+                            price: product.price,
+                            type: product.type,
+                            categories: product.categories,
+                            salePrice: null,
+                          );
+                        },
+                        // count: product.quantity,
+                        count: cartController.cart.data != null &&
+                                cartController
+                                        .cart.data?.products?[product.id] !=
+                                    null
+                            ? cartController
+                                .cart.data?.products![product.id]!.quantity
+                            : 0,
                       ),
                     ),
-                  ],
-                ),
-                Container(
-                  alignment: Alignment.topLeft,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text('to_cart'.tr),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/screen/wishlist/wishlist_screen.dart
+++ lib/screen/wishlist/wishlist_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tajir/base/statefull_data.dart';
+import 'package:tajir/controller/account_login_status_controller.dart';
+import 'package:tajir/controller/wishlist_controller.dart';
 import 'package:tajir/screen/wishlist/local_widgets/wishlist_app_bar.dart';
 import 'package:tajir/theme/app_dimension.dart';
+import 'package:tajir/util/interpolation_util.dart';
+import 'package:tajir/widget/error_screen.dart';
 
 import 'local_widgets/wishlist_product_item.dart';
 
@@ -9,37 +15,115 @@ class WishlistScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        const WishlistAppBar(),
-        SliverToBoxAdapter(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-                horizontal: AppDimension.paddingMedium),
-            child: Text(
-              '5 товаров',
-              style: Theme.of(context).textTheme.bodyText1,
+    return GetBuilder<WishlistController>(
+      builder: (controller) {
+        bool isLoggedIn =
+            controller.accountLoginStatus == AccountLoginStatus.loggedIn;
+        return CustomScrollView(
+          slivers: [
+            const WishlistAppBar(),
+            SliverVisibility(
+              visible: controller.accountLoginStatus ==
+                  AccountLoginStatus.notLoggedIn,
+              sliver: SliverFillRemaining(
+                hasScrollBody: false,
+                child: Center(
+                  child: Text('user_not_logged_in'.tr),
+                ),
+              ),
             ),
-          ),
-        ),
-        const SliverList(
-          delegate: SliverChildListDelegate.fixed(
-            [
-              WishlistProductItem(
-                isShadowVisible: false,
+            SliverVisibility(
+              visible:
+                  isLoggedIn && controller.wishList.status == Status.completed,
+              sliver: SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: AppDimension.paddingMedium,
+                      vertical: AppDimension.marginMedium),
+                  child: Text(
+                    InterpolationUtil.interpolate("product_count".tr,
+                        [controller.wishList.data?.length ?? 0]),
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                ),
               ),
-              WishlistProductItem(),
-              WishlistProductItem(
-                isShadowVisible: false,
+            ),
+            SliverVisibility(
+              visible: isLoggedIn &&
+                  controller.wishList.status == Status.completed &&
+                  controller.wishList.data != null &&
+                  controller.wishList.data!.isEmpty,
+              sliver: SliverFillRemaining(
+                hasScrollBody: false,
+                child: Center(
+                  child: Text('wishlist_is_empty'.tr),
+                ),
               ),
-              WishlistProductItem(),
-              WishlistProductItem(
-                isShadowVisible: false,
+            ),
+            SliverVisibility(
+              visible: isLoggedIn &&
+                  controller.wishList.status == Status.completed &&
+                  controller.wishList.data != null &&
+                  controller.wishList.data!.isNotEmpty,
+              sliver: SliverList(
+                delegate: SliverChildListDelegate(
+                  controller.wishList.data != null
+                      ? controller.wishList.data!.entries
+                          .map((product) => WishlistProductItem(
+                                product: product.value,
+                                onRemoveTapped: () {
+                                  controller.removeProduct(product.value.id);
+                                },
+                                onProductTapped: () {
+                                  controller.onProductTapped(product.value.id);
+                                },
+                              ))
+                          .toList()
+                      : [],
+                ),
               ),
-            ],
-          ),
-        ),
-      ],
+            ),
+            SliverVisibility(
+              visible:
+                  controller.accountLoginStatus == AccountLoginStatus.loading ||
+                      controller.wishList.status == Status.loading,
+              sliver: const SliverFillRemaining(
+                hasScrollBody: false,
+                child: Center(
+                  child: CircularProgressIndicator.adaptive(),
+                ),
+              ),
+            ),
+            SliverVisibility(
+              visible:
+                  controller.accountLoginStatus == AccountLoginStatus.error,
+              sliver: SliverFillRemaining(
+                hasScrollBody: false,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('error_getting_account'.tr),
+                    ElevatedButton(
+                        onPressed: () {
+                          controller.onAccountRetryTapped();
+                        },
+                        child: Text('retry'.tr)),
+                  ],
+                ),
+              ),
+            ),
+            SliverVisibility(
+              visible: controller.wishList.status == Status.error,
+              sliver: SliverFillRemaining(
+                hasScrollBody: false,
+                child: ErrorScreen(onRetryTapped: () {
+                  controller.fetchWishList();
+                }),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
