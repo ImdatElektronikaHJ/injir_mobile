046d8104ec49957c7b639d12c78cfc27e1d9b104
purpurni <sekisPurp@gmail.com>
2022-06-23 17:42:53 +0500

    updated UI for cart

--- lib/screen/cart/cart_screen.dart
+++ lib/screen/cart/cart_screen.dart
@@ -6,14 +6,13 @@ import 'package:tajir/const/nested_navigation_ids.dart';
 import 'package:tajir/controller/cart_controller.dart';
 import 'package:tajir/data/network/helpers/cart_sync_helper.dart';
 import 'package:tajir/model/cart.dart';
-import 'package:tajir/screen/cart/local_widgets/cart_product_item.dart';
 import 'package:tajir/screen/cart/local_widgets/cart_total_bar.dart';
-import 'package:tajir/screen/product/product_screen.dart';
-import 'package:tajir/theme/app_dimension.dart';
-import 'package:tajir/util/interpolation_util.dart';
 import 'package:tajir/widget/error_screen.dart';
 
+import '../../theme/app_dimension.dart';
+import '../../util/interpolation_util.dart';
 import 'local_widgets/cart_app_bar.dart';
+import 'local_widgets/cart_product_item.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({Key? key}) : super(key: key);
@@ -21,117 +20,109 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<CartController>(
-      // init: CartController(),
       builder: (controller) {
         Cart? cart = controller.cart.data ?? Cart();
         return Column(
           children: [
-            const CartAppBar(),
-            Visibility(
-              visible: controller.cart.status == Status.completed,
-              child: Expanded(
-                child: Column(
-                  children: [
-                    Expanded(
-                      child: CustomScrollView(
-                        slivers: [
-                          SliverToBoxAdapter(
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: AppDimension.paddingMedium, vertical: AppDimension.paddingSmall),
-                              child: Text(
-                                InterpolationUtil.interpolate(
-                                    "product_count".tr,
-                                    [cart.products?.length ?? 0]),
-                                style: Theme.of(context).textTheme.bodyText1,
-                              ),
-                            ),
-                          ),
-                          SliverVisibility(
-                            visible: cart.products == null,
-                            sliver: SliverFillRemaining(
-                              hasScrollBody: false,
-                              child: Center(
-                                child: Text('cart_is_empty'.tr),
-                              ),
-                            ),
-                          ),
-                          SliverVisibility(
-                            visible: cart.products != null,
-                            sliver: SliverList(
-                              delegate: SliverChildListDelegate(
-                                cart.products == null
-                                    ? []
-                                    : cart.products!.entries
-                                        .map(
-                                          (entryValue) => SizedBox(
-                                            height: 200.0,
-                                            width: 200.0,
-                                            child: CartProductItem(
-                                                onProductTapped: () {
-                                                  Get.toNamed(
-                                                      AppRoutes.productRoute,
-                                                      id: NestedNavigationIds
-                                                          .cart,
-                                                      arguments: {
-                                                        productId:
-                                                            entryValue.value.id
-                                                      });
-                                                },
-                                                onIncrementTapped: () {
-                                                  controller.incrementTapped(
-                                                      entryValue.value.id);
-                                                },
-                                                onDecrementTapped: () {
-                                                  controller.decrementTapped(
-                                                      entryValue.value.id);
-                                                },
-                                                isShadowVisible:
-                                                    entryValue.key % 2 == 0,
-                                                cartProduct: entryValue.value,
-                                                onDeleteTapped: () {
-                                                  controller.removeProduct(entryValue.value.id);
-                                                }),
-                                          ),
-                                        )
-                                        .toList(),
-                              ),
-                            ),
-                          ),
-                        ],
+            Expanded(
+              child: CustomScrollView(
+                slivers: [
+                  const CartAppBar(),
+                  SliverVisibility(
+                    visible: controller.cart.status == Status.completed,
+                    sliver: SliverToBoxAdapter(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: AppDimension.paddingMedium,
+                            vertical: AppDimension.paddingSmall),
+                        child: Text(
+                          InterpolationUtil.interpolate(
+                              "product_count".tr, [cart.products?.length ?? 0]),
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
                       ),
                     ),
-                    CartTotalBar(
-                        total: cart.total.toString(),
-                        onCheckoutTapped: () {
-                          if (controller.syncStatus != CartSyncStatus.loading &&
-                              controller.cart.status != Status.loading) {
-                            Get.toNamed(AppRoutes.checkoutRoute,
-                                id: NestedNavigationIds.cart);
-                          }
+                  ),
+                  SliverVisibility(
+                    visible: controller.cart.status == Status.completed &&
+                        cart.products == null,
+                    sliver: SliverFillRemaining(
+                      hasScrollBody: false,
+                      child: Center(
+                        child: Text('cart_is_empty'.tr),
+                      ),
+                    ),
+                  ),
+                  SliverVisibility(
+                    visible: controller.cart.status == Status.completed &&
+                        cart.products != null,
+                    sliver: SliverList(
+                      delegate: SliverChildListDelegate(
+                        cart.products == null
+                            ? []
+                            : cart.products!.entries
+                                .map(
+                                  (entryValue) => CartProductItem(
+                                      onProductTapped: () {
+                                        controller.onProductTapped(
+                                            entryValue.value.id);
+                                      },
+                                      onIncrementTapped: () {
+                                        controller.incrementTapped(
+                                            entryValue.value.id);
+                                      },
+                                      onDecrementTapped: () {
+                                        controller.decrementTapped(
+                                            entryValue.value.id);
+                                      },
+                                      isShadowVisible: entryValue.key % 2 == 0,
+                                      cartProduct: entryValue.value,
+                                      onDeleteTapped: () {
+                                        controller
+                                            .removeProduct(entryValue.value.id);
+                                      }),
+                                )
+                                .toList(),
+                      ),
+                    ),
+                  ),
+                  SliverVisibility(
+                    visible: controller.cart.status == Status.loading,
+                    sliver: const SliverFillRemaining(
+                      hasScrollBody: false,
+                      child: Center(
+                        child: CircularProgressIndicator.adaptive(),
+                      ),
+                    ),
+                  ),
+                  SliverVisibility(
+                    visible: controller.cart.status == Status.error,
+                    sliver: SliverFillRemaining(
+                      hasScrollBody: false,
+                      child: ErrorScreen(
+                        onRetryTapped: () {
+                          controller.fetchCart();
                         },
-                        isLoading:
-                            controller.syncStatus == CartSyncStatus.loading ||
-                                controller.cart.status == Status.loading),
-                  ],
-                ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
             Visibility(
-              visible: controller.cart.status == Status.loading,
-              child: const Expanded(
-                child: Center(
-                  child: CircularProgressIndicator.adaptive(),
-                ),
-              ),
-            ),
-            Visibility(
-                visible: controller.cart.status == Status.error,
-                child: ErrorScreen(
-                  onRetryTapped: () {
-                    controller.fetchCart();
+              visible: controller.cart.status == Status.completed,
+              child: CartTotalBar(
+                  total: cart.total.toString(),
+                  onCheckoutTapped: () {
+                    if (controller.syncStatus != CartSyncStatus.loading &&
+                        controller.cart.status != Status.loading) {
+                      Get.toNamed(AppRoutes.checkoutRoute,
+                          id: NestedNavigationIds.cart);
+                    }
                   },
-                )),
+                  isLoading: controller.syncStatus == CartSyncStatus.loading ||
+                      controller.cart.status == Status.loading),
+            ),
           ],
         );
       },
